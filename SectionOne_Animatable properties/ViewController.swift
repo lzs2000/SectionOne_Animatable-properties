b1e5d1704b02fd63b13697b3a2db0a2766031326
lzs2000 <jia2000ning@sina.com>
2017-06-06 17:23:36 +0800

    an activity indicator
    给登录按钮添加an activity indicator

--- SectionOne_Animatable properties/ViewController.swift
+++ SectionOne_Animatable properties/ViewController.swift
@@ -21,11 +21,18 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var loginButton: UIButton!
     
+    let spinner = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
          print("\(#function) in \(object_getClassName(self))")
         
-        
+        self.spinner.frame = CGRect(x: -20, y: 0, width: 20, height: 20)
+        self.spinner.startAnimating()
+        self.spinner.alpha = 0.0
+        self.loginButton.addSubview(spinner)
         
         /* here are the properties you can use to modify a view's position and size
          bounds: Animate this property to reposition the view's content within the view's frame
@@ -56,6 +63,7 @@ class ViewController: UIViewController {
         
         loginButton.center.y += 30.0
         loginButton.alpha = 0.0
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -92,12 +100,24 @@ class ViewController: UIViewController {
         UIView.animate(withDuration: 3.0, delay: 0.5, usingSpringWithDamping: 0.5, initialSpringVelocity:0,   options: [], animations: {
             self.loginButton.center.y -= 30.0
             self.loginButton.alpha = 1.0
+            
+            //an activity indicator
+            self.spinner.center = CGPoint(x: 40.0, y: self.loginButton.frame.size.height / 2)
+            
         }, completion: nil)
         
-        
-        
+       
     }
-
+    @IBAction func login(_ sender: Any) {
+        UIView.animate(withDuration: 1.5, delay: 0.0, usingSpringWithDamping:0.2, initialSpringVelocity:0.0,  options: [], animations: {
+            self.loginButton.bounds.size.width += 80
+        }, completion: nil)
+        UIView.animate(withDuration: 0.33, delay: 0.0, usingSpringWithDamping:0.7, initialSpringVelocity: 0.0,  options: [], animations: {
+            self.loginButton.center.y += 60
+            self.loginButton.backgroundColor = UIColor(red: 0.85, green: 0.83, blue: 0.45, alpha: 1.0)
+            self.spinner.alpha = 1.0
+        }, completion: nil)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
          print("\(#function) in \(object_getClassName(self))")
